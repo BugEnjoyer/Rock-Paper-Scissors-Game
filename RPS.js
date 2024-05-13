0564dcd32d18f85d5288a07b0b9772a3c38df892
kevinR <kevinroesner94@gmail.com>
2024-05-14 00:40:34 +0200

    Made RPS working but score still left

--- RPS.js
+++ RPS.js
@@ -1,6 +1,8 @@
 let computerChoice= '';
-let randomNumber= Math.random();
+let humanChoice='';
+let result='';
 function getComputerChoice() {
+  let randomNumber= Math.random();
   if (randomNumber <= 1/3 ) {
     computerChoice= 'rock';
   } else if ( randomNumber <= 2/3 && randomNumber > 1/3) {
@@ -10,3 +12,32 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
+
+function getResult(humanChoice, computerChoice) {
+  if ( humanChoice === computerChoice ) {
+   result = 'tie!';
+  } else if ( humanChoice === 'rock' && computerChoice === 'scissors') {
+     result = 'You win!';
+  }else if ( humanChoice === 'paper' && computerChoice === 'scissors') {
+     result = 'You lose!';
+  }else if ( humanChoice === 'rock' && computerChoice === 'paper') {
+     result = 'You lose!';
+  }else if ( humanChoice === 'paper' && computerChoice === 'rock') {
+     result = 'You win!';
+  }else if ( humanChoice === 'scissors' && computerChoice === 'rock') {
+     result = 'You lose!';
+  }else if ( humanChoice === 'scissors' && computerChoice === 'paper') {
+     result = 'You win!';
+  }
+  console.log(result);
+  return result;
+}
+
+function playRound() {
+  getComputerChoice();
+  getResult(humanChoice, computerChoice);
+    document.querySelector('.js-result-para')
+    .innerHTML= `Result: You chose ${humanChoice} and computer chose ${computerChoice}.
+    ${result}`
+}
+
