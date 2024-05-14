c9342ec55f3fdf09e8ac8722c96220c1fb0ac91b
kevinR <kevinroesner94@gmail.com>
2024-05-14 17:31:26 +0200

    Done with the RPS Game and everything works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Rock-Paper-Scissors-Game
+
+The first JS project from me.
+A simple and nostalgic Rock-Paper-Scissors Game.
+
+Have fun testing your luck!
--- RPS.js
+++ RPS.js
@@ -1,6 +1,12 @@
 let computerChoice= '';
 let humanChoice='';
 let result='';
+
+score={
+   Wins: 0,
+   Losses: 0
+};
+
 function getComputerChoice() {
   let randomNumber= Math.random();
   if (randomNumber <= 1/3 ) {
@@ -33,11 +39,60 @@ function getResult(humanChoice, computerChoice) {
   return result;
 }
 
+function updateScore() {
+   if( result === 'You win!' && score.Wins < 5) {
+      score.Wins ++;
+     } else if ( result === 'You lose!' && score.Losses < 5) {
+      score.Losses ++;
+     }
+    if ( score.Wins === 0) {
+      document.querySelector('.js-win-para')
+     .innerHTML='Wins: 0';
+     }else if ( score.Wins === 5) {
+      document.querySelector('.js-win-lose')
+      .innerHTML='Gratulation! You have won!';
+      document.querySelector('.new-match-button').innerHTML='New Match';
+      document.querySelector('.new-match-button').style.opacity= 1;
+      document.querySelector('.js-win-para')
+     .innerHTML=`Wins: ${score.Wins}`;
+    } else {
+      document.querySelector('.js-win-para')
+     .innerHTML=`Wins: ${score.Wins}`;
+    }
+    if ( score.Losses === 0) {
+      document.querySelector('.js-lose-para')
+     .innerHTML='Losses: 0';
+   }else if ( score.Losses === 5) {
+      document.querySelector('.js-win-lose')
+      .innerHTML='Game Over! You have lost!';
+      document.querySelector('.new-match-button').innerHTML='New Match';
+      document.querySelector('.new-match-button').style.opacity= 1;
+      document.querySelector('.js-lose-para')
+     .innerHTML=`Wins: ${score.Losses}`;
+    } else {
+      document.querySelector('.js-lose-para')
+     .innerHTML=`Losses: ${score.Losses}`;
+    }
+      
+}
+
 function playRound() {
   getComputerChoice();
   getResult(humanChoice, computerChoice);
+  updateScore();
     document.querySelector('.js-result-para')
     .innerHTML= `Result: You chose ${humanChoice} and computer chose ${computerChoice}.
     ${result}`
+}
+
+function resetScore() {
+   score={
+      Wins: 0,
+      Losses: 0
+   };
+   document.querySelector('.js-win-para')
+   .innerHTML = 'Wins: 0';
+   document.querySelector('.js-lose-para')
+   .innerHTML = 'Losses: 0';
 }
 
